e0da0703a9337feb823970d878590cab07d07b7d
jacob-sasser <44883537+jacob-sasser@users.noreply.github.com>
2019-02-16 13:49:27 -0500

    broken auto climb code

--- Astro/commands/climber/autoClimb.py
+++ Astro/commands/climber/autoClimb.py
@@ -1,30 +1,30 @@
 import wpilib
 from wpilib.command import Command
 from wpilib.command import TimedCommand
+from commands.climber import liftRobot
 
 class AutoClimb(TimedCommand):
 
     def __init__(self):
         super().__init__('AutoClimb')
-
+        self.frontSensor = #switch syntax
+        self.backSensor = #switch syntax
+        self.frontEncoder = #encoder syntax
+        self.backEncoder = #encoder syntax
         self.requires(self.getRobot().drive)
         self.requires(self.getRobot().climber)
         self.climber = self.getRobot().climber
         self.DT = self.getRobot().drive
-
-
+        self.LiftRobot = commands.climber.liftRobot.LiftRobot
 
     def initialize(self):
         pass
 
     def execute(self):
-        self.time = wpilib.time()
-        
-        self.climber.lift(0.9)
-        self.climber.wheelForward()
-        self.climber.lift(-0.4)
-        self.climber.liftFront(-0.9)
-
+        while not self.climber.isFullyExtendedFront():
+            self.LiftRobot('both')
+        while not forwardSensor:
+            self.LiftRobot('')
     def isFinished(self):
         pass
         
