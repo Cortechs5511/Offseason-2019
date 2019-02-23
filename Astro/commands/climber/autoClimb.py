8efcdabf2d901bf977f4abcf6435baa6c4c1ab65
spasumarthy <sushritp1@gmail.com>
2019-02-23 10:56:59 -0500

    autoClimb fixed

--- Astro/commands/climber/autoClimb.py
+++ Astro/commands/climber/autoClimb.py
@@ -3,17 +3,20 @@ from wpilib.command import Command
 from wpilib.command import TimedCommand
 from wpilib.command import CommandGroup
 from commands.climber import driveToEdge
-
-
+from commands.climber import liftRobot 
+from commands.drive import setFixedDT 
 
 class AutoClimb(CommandGroup):
     def __init__(self):
         self.LiftRobot = liftRobot.LiftRobot
         self.DriveToEdge = driveToEdge.DriveToEdge
-        self.addSequential(LiftRobot("both"))
+        self.SetFixedDT = setFixedDT.SetFixedDT  
+        
+    def execute(self):
+        self.addSequential(self.LiftRobot("both"))
         self.addSequential(self.DriveToEdge("front"))
         self.addSequential(self.LiftRobot("front"))
         self.addSequential(self.DriveToEdge("back"))
         self.addSequential(self.LiftRobot("back"))
         # time units is in milliseconds
-        self.addSequential(self.commands.drive.setFixedDT.SetFixedDT(0.5, 0.5, 200))
+        self.addSequential(self.SetFixedDT(0.5, 0.5, 200))
