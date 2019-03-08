d92429d56556e94126f36c02f3bab91551aba9b9
avgupta456 <avgupta456@gmail.com>
2019-03-08 01:05:19 -0500

    hatch in auto

--- Wake/commands/autonomous.py
+++ Wake/commands/autonomous.py
@@ -3,87 +3,96 @@ from wpilib.command import CommandGroup
 from commands.drive import driveStraightCombined
 from commands.drive import turnAngle
 
+from subsystems import HatchMech
+
 class DriveStraight(CommandGroup):
     def __init__(self):
         super().__init__('DriveStraight')
         DriveStraightCombined = driveStraightCombined.DriveStraightCombined
+        
         self.addSequential(DriveStraightCombined(distance=212.8, angle=0, timeout=5))
-
 
 class LeftCargo(CommandGroup):
     def __init__(self):
         super().__init__('LeftCargo')
         DriveStraightCombined = driveStraightCombined.DriveStraightCombined
         TurnAngle = turnAngle.TurnAngle
-        #EjectHatch = ejectHatch.EjectHatch
+        EjectHatch = HatchMech.EjectHatch
+
         self.addSequential(DriveStraightCombined(distance=212.8, angle=0, timeout=5))
         self.addSequential(TurnAngle(angle=90, timeout=5))
         self.addSequential(DriveStraightCombined(distance=21.13, angle=90, timeout=5))
-        #self.addSequential(EjectHatch())
+        self.addSequential(EjectHatch())
 
 class RightCargo(CommandGroup):
     def __init__(self):
         super().__init__('RightCargo')
         DriveStraightCombined = driveStraightCombined.DriveStraightCombined
         TurnAngle = turnAngle.TurnAngle
-        #EjectHatch = ejectHatch.EjectHatch
+        EjectHatch = HatchMech.EjectHatch
+
         self.addSequential(DriveStraightCombined(distance=212.8, angle=0, timeout=5))
         self.addSequential(TurnAngle(angle=-90, timeout=5))
         self.addSequential(DriveStraightCombined(distance=21.13, angle=-90, timeout=5))
-        #self.addSequential(EjectHatch())
+        self.addSequential(EjectHatch())
 
 class CenterCargo(CommandGroup):
     def __init__(self):
         super().__init__('CenterCargo')
         DriveStraightCombined = driveStraightCombined.DriveStraightCombined
-        #EjectHatch = ejectHatch.EjectHatch
+        EjectHatch = HatchMech.EjectHatch
+
         self.addSequential(DriveStraightCombined(distance=173.25, angle=0, timeout=5))
-        #self.addSequential(EjectHatch())
+        self.addSequential(EjectHatch())
 
 class LeftCargoLevel2(CommandGroup):
     def __init__(self):
         super().__init__('LeftCargoLevel2')
         DriveStraightCombined = driveStraightCombined.DriveStraightCombined
         TurnAngle = turnAngle.TurnAngle
-        #EjectHatch = ejectHatch.EjectHatch
+        EjectHatch = HatchMech.EjectHatch
+
         self.addSequential(DriveStraightCombined(distance=271, angle=0, timeout=5))
         self.addSequential(TurnAngle(angle=90, timeout=5))
         self.addSequential(DriveStraightCombined(distance=17.6, angle=90, timeout=5))
-        #self.addSequential(EjectHatch())
+        self.addSequential(EjectHatch())
 
 class RightCargoLevel2(CommandGroup):
     def __init__(self):
         super().__init__('RightCargoLevel2')
         DriveStraightCombined = driveStraightCombined.DriveStraightCombined
         TurnAngle = turnAngle.TurnAngle
-        #EjectHatch = ejectHatch.EjectHatch
+        EjectHatch = HatchMech.EjectHatch
+
         self.addSequential(DriveStraightCombined(distance=271, angle=0, timeout=5))
         self.addSequential(TurnAngle(angle=-90, timeout=5))
         self.addSequential(DriveStraightCombined(distance=17.6, angle=-90, timeout=5))
-        #self.addSequential(EjectHatch())
+        self.addSequential(EjectHatch())
 
 class CenterCargoLevel2Left(CommandGroup):
     def __init__(self):
         super().__init__('CenterCargoLevel2Left')
         DriveStraightCombined = driveStraightCombined.DriveStraightCombined
         TurnAngle = turnAngle.TurnAngle
-        #EjectHatch = ejectHatch.EjectHatch
+        EjectHatch = HatchMech.EjectHatch
+
         self.addSequential(DriveStraightCombined(distance=153.63, angle=0, timeout=5))
         self.addSequential(TurnAngle(angle=90, timeout=5))
         self.addSequential(DriveStraightCombined(distance=28.87, angle=90, timeout=5))
         self.addSequential(TurnAngle(angle=0, timeout=5))
         self.addSequential(DriveStraightCombined(distance=19.12, angle=0, timeout=5))
-        #self.addSequential(EjectHatch())
+        self.addSequential(EjectHatch())
 
 class CenterCargoLevel2Right(CommandGroup):
     def __init__(self):
         super().__init__('CenterCargoLevel2Right')
         DriveStraightCombined = driveStraightCombined.DriveStraightCombined
         TurnAngle = turnAngle.TurnAngle
-        #EjectHatch = ejectHatch.EjectHatch
+        EjectHatch = HatchMech.EjectHatch
+
         self.addSequential(DriveStraightCombined(distance=153.63, angle=0, timeout=5))
         self.addSequential(TurnAngle(angle=-90, timeout=5))
         self.addSequential(DriveStraightCombined(distance=28.87, angle=-90, timeout=5))
         self.addSequential(TurnAngle(angle=0, timeout=5))
         self.addSequential(DriveStraightCombined(distance=19.12, angle=0, timeout=5))
-        #self.addSequential(EjectHatch())
+        self.addSequential(EjectHatch())
--- Wake/subsystems/HatchMech.py
+++ Wake/subsystems/HatchMech.py
@@ -1,10 +1,11 @@
 from wpilib import SmartDashboard
 from wpilib import Solenoid
+
 from wpilib.command.subsystem import Subsystem
 from wpilib.command import Command
+
 import map
 import oi
-
 
 class HatchMech(Subsystem):
     def __init__(self, Robot):
@@ -27,18 +28,32 @@ class HatchMech(Subsystem):
         elif mode == "out": self.piston.set(False)
         else: self.piston.set(not self.piston.get())
 
-    def isEjectorOut(self):
-        return self.piston.get()
+    def isEjectorOut(self): return self.piston.get()
 
     def toggle(self):
         if self.piston.get(): self.kick("out")
         else: self.kick("in")
 
-    def disable(self):
-        self.kick("in")
+    def disable(self): self.kick("in")
 
-    def dashboardInit(self):
-        pass
+    def dashboardInit(self): pass
 
-    def dashboardPeriodic(self):
-        pass
+    def dashboardPeriodic(self): pass
+
+class EjectHatch(Command):
+    def __init__(self):
+        super().__init__('EjectHatch')
+        robot = self.getRobot()
+        self.hatch = robot.hatch
+        self.requires(self.hatch)
+
+    def initialize(self): pass
+
+    def execute(self): self.hatch.kick("out")
+
+
+    def isFinished(self): return self.timeSinceInitialized()>0.25
+
+    def interrupted(self): pass
+
+    def end(self): self.hatch.kick("in")
