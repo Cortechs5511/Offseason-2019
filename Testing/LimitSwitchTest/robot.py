cf2e1ac69659c6a05ecd7f30800dd33bb77fdae7
Hyperwizard42 <44416801+Hyperwizard42@users.noreply.github.com>
2019-03-16 14:49:50 -0400

    Testing for limit switches

---
+++ Testing/LimitSwitchTest/robot.py
@@ -0,0 +1,21 @@
+import wpilib
+from wpilib import TimedRobot
+from wpilib import SmartDashboard as sd
+from ctre import WPI_TalonSRX as Talon
+from wpilib import Joystick
+
+class MyRobot(TimedRobot):
+    def robotInit(self):
+        self.joystick  = Joystick(0)
+        self.motor = Talon(10)
+    def teleopPeriodic(self):
+        self.y = self.joystick.getY()
+        if abs(self.y) <0.1:
+            self.y = 0
+        self.motor.set(self.y)
+    def robotPeriodic(self):
+        sd.putNumber('Forward closed',self.motor.isFwdLimitSwitchClosed())
+        sd.putNumber('Back closed', self.motor.isRevLimitSwitchClosed())
+        sd.putNumber('motor position', self.motor.getQuadraturePosition())
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
