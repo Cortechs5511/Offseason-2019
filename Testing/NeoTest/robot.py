a922f871b4c9de76e900fb733a4ab8445880a617
Paul Blankenbaker <paul.blankenbaker@gmail.com>
2019-03-26 19:18:38 -0400

    Updated Spark Max/Neo Test for PWM style connection

--- Testing/NeoTest/robot.py
+++ Testing/NeoTest/robot.py
@@ -1,5 +1,6 @@
 import wpilib
 from wpilib import SmartDashboard as sd
+from wpilib import Spark
 import rev
 from rev import CANSparkMax
 
@@ -7,12 +8,14 @@ class MyRobot(wpilib.TimedRobot):
     """ Small program to verify Neo motor and encoder are working. """
 
     def robotInit(self):
-      self.neo: CANSparkMax = CANSparkMax(5, rev.MotorType.kBrushless)
+      #self.neo: CANSparkMax = CANSparkMax(5, rev.MotorType.kBrushless)
+      self.neo: Spark = Spark(0)
       sd.putNumber("Neo Power", 0.5)
 
     def robotPeriodic(self):
-      sd.putNumber("Neo Encoder", self.neo.getEncoder().getPosition())
-        
+      #sd.putNumber("Neo Encoder", self.neo.getEncoder().getPosition())
+      pass
+      
     def teleopPeriodic(self):
       self.neo.set(sd.getNumber("Neo Power", 0.5))
 
