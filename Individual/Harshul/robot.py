0a8cf33aed0fa53a3fe12453b917b7eab76f714d
AshishGupta938 <ashvgupta938@gmail.com>
2019-09-08 16:25:47 -0400

    Set up Individual Folders
    This is the first commit in Offseason-2019. I added individual folders for Archit, Eric, and Harshul to use during programming training. Each one has a template robot.py file

---
+++ Individual/Harshul/robot.py
@@ -0,0 +1,12 @@
+import wpilib
+
+class MyRobot(wpilib.TimedRobot):
+
+    def robotInit(self):
+        pass
+
+    def teleopPeriodic(self):
+        pass
+
+if __name__ == '__main__':
+    wpilib.run(MyRobot)
