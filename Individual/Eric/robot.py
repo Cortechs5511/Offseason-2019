30fc68608117aab22ef4649043a4463b1d590ddb
ericliu0000 <12389643+ericliu0000@users.noreply.github.com>
2019-09-23 17:19:04 -0400

    Updated individual file
    Refined code that is now able to run in the simulator
    - Changed motor controllers
    - Deprecated motor control group in lieu of individual motor controller commands (temporary)
    - The code now does not raise fatal errors and appears to work in the simulator.

--- Individual/Eric/robot.py
+++ Individual/Eric/robot.py
@@ -1,23 +1,20 @@
 import wpilib
 import wpilib.drive
-
+import ctre
 
 class MyRobot(wpilib.TimedRobot):
-
     def robotInit(self):
-        self.frontLeft = wpilib.Spark(1)
-        self.midLeft = wpilib.Spark(2)
-        self.rearLeft = wpilib.Spark(3)
-        self.left = wpilib.SpeedControllerGroup(self.frontLeft, self.midLeft, self.rearLeft)
+        self.frontLeft = ctre.WPI_TalonSRX(0) #can't get talons to work
+        self.rearLeft = ctre.WPI_TalonSRX(1)
+        #self.left = wpilib.SpeedControllerGroup(self.frontLeft, self.rearLeft)
 
-        self.frontRight = wpilib.Spark(4)
-        self.midRight = wpilib.Spark(5)
-        self.rearRight = wpilib.Spark(6)
-        self.right = wpilib.SpeedControllerGroup(self.frontRight, self.midRight, self.rearRight)
+        self.frontRight = ctre.WPI_TalonSRX(2)
+        self.rearRight = ctre.WPI_TalonSRX(3)
+        #self.right = wpilib.SpeedControllerGroup(self.frontRight, self.rearRight)
 
-        self.drive = DifferentialDrive(self.left, self.right) # lines 8-18 are from documentation
-        self.leftStick = wpilib.Joystick(1)
-        self.rightStick = wpilib.Joystick(2)
+        #self.drive = wpilib.DifferentialDrive(self.left, self.right)
+        self.leftStick = wpilib.Joystick(0)
+        self.rightStick = wpilib.Joystick(1)
         self.timer = wpilib.Timer()
 
     def autonomousInit(self):
@@ -26,18 +23,41 @@ class MyRobot(wpilib.TimedRobot):
         self.timer.start()
 
     def autonomousPeriodic(self):
-        """This function is called periodically during autonomous."""
+        """This function is called periodically during autonomous. Please don't select this one, because it instantly kills the robot."""
+        print("This is a placeholder. It is recommended to stop this.")
+        self.frontRight.set(-0.01)
+        self.rearRight.set(0.01)
+        self.frontLeft.set(0.01)
+        self.rearLeft.set(-0.01)
 
-        # Drive for two seconds
-        if self.timer.get() < 2.0:
-            self.drive.tankDrive(0.5, 0.5, True)  # Drive forwards at half speed
-        elif self.leftStick.getY() == 0 and self.rightStick.getY() == 0: # using this in ideal situation with perfect joysticks
-            self.drive.arcadeDrive(-1, 1)  # Turn left in circles at max speed (not very good)
 
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
-        self.drive.tankDrive(self.leftStick.getY(), self.rightStick.getY()) # I think this is how tank driving works, y-axis control on both joysticks
+        #ideally this would be in another file but i can't figure out how to do that yet
+        if abs(self.rightStick.getY()) < 0.05:
+            self.frontRight.set(0)
+            self.rearRight.set(0)
+            pass
+        if abs(self.leftStick.getY()) < 0.05:
+            self.frontLeft.set(0)
+            self.rearLeft.set(0)
+            pass
+        if self.rightStick.getY() >= 0.05:
+            self.frontRight.set(self.rightStick.getY()*0.9) #5% deadband for forwards and reverse
+            self.rearRight.set(self.rightStick.getY()*0.9) #0.9x linear scaling
+        elif self.rightStick.getY() <= -0.05:
+            self.frontRight.set(self.rightStick.getY()*0.9)
+            self.rearRight.set(self.rightStick.getY()*0.9)
+        if self.leftStick.getY() >= 0.05:
+            self.frontLeft.set(self.leftStick.getY()*0.9) #5% deadband for forwards and reverse
+            self.rearLeft.set(self.leftStick.getY()*0.9) #0.9x linear scaling
+        elif self.leftStick.getY() <= -0.05:
+            self.frontLeft.set(self.leftStick.getY()*0.9)
+            self.rearLeft.set(self.leftStick.getY()*0.9)
 
 
 if __name__ == "__main__":
     wpilib.run(MyRobot)
+
+#to-do work out motor control groups to optimize teleopPeriodic
+#to-do physics file
