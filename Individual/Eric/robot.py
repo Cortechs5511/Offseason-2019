ed75226d784310886ea0dd142aed662d6cd0e858
ericliu0000 <12345678+ericliu0000@users.noreply.github.com>
2019-09-23 01:25:35 -0400

    Code in Eric's file (re-try)
    In the initial push, it did not work. This is my second attempt to get the repository to update.
    - Imported tank steering and example code
    - Tweaked it a little bit
    - In general explored some commands

--- Individual/Eric/robot.py
+++ Individual/Eric/robot.py
@@ -5,9 +5,34 @@ import wpilib.drive
 class MyRobot(wpilib.TimedRobot):
 
     def robotInit(self):
-        #init motor controllers
-        #init joysticks
-        pass
+        self.frontLeft = wpilib.Spark(1)
+        self.midLeft = wpilib.Spark(2)
+        self.rearLeft = wpilib.Spark(3)
+        self.left = wpilib.SpeedControllerGroup(self.frontLeft, self.midLeft, self.rearLeft)
+
+        self.frontRight = wpilib.Spark(4)
+        self.midRight = wpilib.Spark(5)
+        self.rearRight = wpilib.Spark(6)
+        self.right = wpilib.SpeedControllerGroup(self.frontRight, self.midRight, self.rearRight)
+
+        self.drive = DifferentialDrive(self.left, self.right) # lines 8-18 are from documentation
+        self.leftStick = wpilib.Joystick(1)
+        self.rightStick = wpilib.Joystick(2)
+        self.timer = wpilib.Timer()
+
+    def autonomousInit(self):
+        """This function is run once each time the robot enters autonomous mode."""
+        self.timer.reset()
+        self.timer.start()
+
+    def autonomousPeriodic(self):
+        """This function is called periodically during autonomous."""
+
+        # Drive for two seconds
+        if self.timer.get() < 2.0:
+            self.drive.tankDrive(0.5, 0.5, True)  # Drive forwards at half speed
+        elif self.leftStick.getY() == 0 and self.rightStick.getY() == 0: # using this in ideal situation with perfect joysticks
+            self.drive.arcadeDrive(-1, 1)  # Turn left in circles at max speed (not very good)
 
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
