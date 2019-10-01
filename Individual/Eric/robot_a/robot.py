60c2fc4f5ee4de01a9a366b5838fc6638e7f5b48
AshishGupta938 <ashvgupta938@gmail.com>
2019-09-30 20:57:21 -0400

    Individual Files Updated
    Individual Files were added to the github.

--- Individual/Eric/robot_a/robot.py
+++ Individual/Eric/robot_a/robot.py
@@ -4,12 +4,14 @@ import wpilib.drive
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
-        self.frontLeft = wpilib.Talon(0)
-        self.rearLeft = wpilib.Talon(1)
+        self.frontLeft = wpilib.Talon(2)
+        self.rearLeft = wpilib.Talon(3)
         self.left = wpilib.SpeedControllerGroup(self.frontLeft, self.rearLeft)
 
-        self.frontRight = wpilib.Talon(2)
-        self.rearRight = wpilib.Talon(3)
+        self.frontRight = wpilib.Talon(0)
+        self.rearRight = wpilib.Talon(1)
+        self.frontRight.setInverted(True)
+        self.rearRight.setInverted(True)
         self.right = wpilib.SpeedControllerGroup(self.frontRight, self.rearRight)
 
         #self.drive = wpilib.TankDrive(self.left, self.right)
@@ -44,8 +46,10 @@ class MyRobot(wpilib.TimedRobot):
     def teleopPeriodic(self):
         leftInput = float(self.leftStick.getY()) * 0.9
         rightInput = float(self.rightStick.getY()) * 0.9
+        #self.left.set(leftInput)
+        #self.right.set(rightInput)
         if abs(leftInput) >= 0.05:
-            self.left.set(-leftInput)
+            self.left.set(leftInput)
         else:
             self.left.set(0)
         if abs(rightInput) >= 0.05:
