531859a5c05c29fbb0d756cfa066c0cf6729cbb4
Isaac <isaacwelborn3@gmail.com>
2019-10-14 20:34:01 -0400

    10/14/19 Continued
    Changing more files!

--- Individual/Isaac/robot.py
+++ Individual/Isaac/robot.py
@@ -1,29 +1,29 @@
-import wpilib
-
 '''Notes'''
-	'''wpilib.DifferentialDrive'''
+'''		wpilib.DifferentialDrive'''
+
+import wpilib
 
 class MyRobot(wpilib.TimedRobot):
 
 	def robotInit(self):
 
-		self.motorL1 = wpilib.Jaguar(0)
-		self.motorL2 = wpilib.Jaguar(1)
-		self.motorR1 = wpilib.Jaguar(2)
-		self.motorR2 = wpilib.Jaguar(3)
-		self.robot_driveL = wpilib.drive.SpeedControllerGroup(self.motorL1, self.motorL2)
-		self.robot_driveR = wpilib.drive.SpeedControllerGroup(self.motorR1, self.motorR2)
-		self.stick1 = wpilib.Joystick(1)
-		self.stick2 = wpilib.Joystick(2)
+		self.motorL1 = wpilib.Talon(0)
+		self.motorL2 = wpilib.Talon(1)
+		self.motorR1 = wpilib.Talon(2)
+		self.motorR2 = wpilib.Talon(3)
+		self.robot_driveL = wpilib.SpeedControllerGroup(self.motorL1, self.motorL2)
+		self.robot_driveR = wpilib.SpeedControllerGroup(self.motorR1, self.motorR2)
+		self.stick1 = wpilib.Joystick(0)
+		self.stick2 = wpilib.Joystick(1)
 
 	def teleopPeriodic(self):
-		speedL = self.stick.getY()
+		speedL = self.stick1.getY()
 		speedR = self.stick2.getY()
-		if abs(self.stick.getY()) < 0.02:
+		if abs(self.stick1.getY()) < 0.02:
 			speedL = 0
 		if abs(self.stick2.getY()) < 0.02:
 			speedR = 0
-		self.robot_driveL.set(self.stick1.getY())
+		self.robot_driveL.set(-self.stick1.getY())
 		self.robot_driveR.set(self.stick2.getY())
 
 if __name__ == "__main__":
