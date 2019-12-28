ddbc73e58c31b0c83b198957ba4e807484ff6f04
ericliu0000 <ericliu1266@gmail.com>
2019-12-28 16:51:50 -0500

    Code for two neos
    Revised code for testing two neos (one side) on the practice drivetrain

--- Testing/NeoTest/robot.py
+++ Testing/NeoTest/robot.py
@@ -8,19 +8,28 @@ class MyRobot(wpilib.TimedRobot):
     """ Small program to verify Neo motor and encoder are working. """
 
     def robotInit(self):
-        self.neo: CANSparkMax = CANSparkMax(3, rev.MotorType.kBrushless)
-        self.neo.clearFaults()
-        #self.neo: Spark = Spark(0)
+        self.neo0: CANSparkMax = CANSparkMax(3, rev.MotorType.kBrushless)
+        self.neo1: CANSparkMax = CANSparkMax(11, rev.MotorType.kBrushless)
+
+        neos = wpilib.SpeedControllerGroup(self.neo0, self.neo1)
+
+        self.neo0.clearFaults()
+        self.neo1.clearFaults()
+
         sd.putNumber("Neo Power", 0)
-        self.neo.setOpenLoopRampRate(5)
+
+        self.neo0.setOpenLoopRampRate(5)
+        self.neo1.setOpenLoopRampRate(5)
 
     def teleopInit(self):
-        self.neo.getEncoder().setPosition(0)
+        self.neo0.getEncoder().setPosition(0)
+        self.neo1.getEncoder().setPosition(0)
 
     def teleopPeriodic(self):
-        sd.putNumber("Neo Encoder", self.neo.getEncoder().getPosition())
-        self.neo.set(sd.getNumber("Neo Power", 0.5))
-        self.neo.set(sd.getNumber("Neo Power", 0))
+        sd.putNumber("Neo Encoder", self.neo0.getEncoder().getPosition())
+        speed = sd.getNumber("Neo Power", 0)
+        self.neo0.set(speed)
+        self.neo1.set(speed)
 
 if __name__ == '__main__':
     wpilib.run(MyRobot)
