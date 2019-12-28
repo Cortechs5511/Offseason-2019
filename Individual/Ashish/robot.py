6e2dc97d5b3faa233e3f95321c08fea85eed5f16
AshishGupta938 <ashvgupta938@gmail.com>
2019-12-28 13:18:24 -0500

    NeoTest Files
    These are the NEO testing files used for testing the NEOs. Limelight files are also included.

--- Individual/Ashish/robot.py
+++ Individual/Ashish/robot.py
@@ -28,8 +28,8 @@ class MyRobot(wpilib.TimedRobot):
         self.table = NetworkTables.getTable('limelight')
 
     def teleopPeriodic(self):
-        leftInput = float(self.leftStick.getY()) * 0.9
-        rightInput = float(self.rightStick.getY()) * 0.9
+        leftInput = float(self.leftStick.getY()) * -0.9
+        rightInput = float(self.rightStick.getY()) * -0.9
         if abs(leftInput) >= 0.05:
             self.left.set(leftInput)
         else:
@@ -39,34 +39,52 @@ class MyRobot(wpilib.TimedRobot):
         else:
             self.right.set(0)
         self.getLimeData()
+        if self.leftStick.getRawButton(1):
+            self.moveRobot()
 
     def getLimeData(self):
         tx = self.table.getNumber('tx', 900)
         ta = self.table.getNumber('ta', 900)
-        distance_1 = (6.5) / (math.sqrt(ta/100) * math.tan(math.radians(29.8)))
-        limeMult = (-5.44 * .0001 * distance_1) + 0.864
-        distance_2 = distance_1 * limeMult
+        ty = self.table.getNumber('ty', 900)
+        stop = False
+        '''if ta != 0:
+            distance_1 = (6.5) / (math.sqrt(ta/100) * math.tan(math.radians(29.8)))
+            limeMult = (-5.44 * .0001 * distance_1) + 0.864
+            distance_2 = distance_1 * limeMult
+        else:
+            distance_2 = 0
+            stop = True'''
+        if ta != 0:
+            distance_2 = 20 / math.tan(math.radians(ty))
+        else:
+            distance_2 = 0
+            stop = True
         SmartDashboard.putNumber("Limelight Distance", distance_2)
         SmartDashboard.putNumber("Limelight Angle", tx)
-        return [distance_2, tx]
+        SmartDashboard.putNumber("Angle of Elevation", ty)
+        return [distance_2, tx, stop]
 
     def moveRobot(self):
-        [distance, angle] = self.getLimeData()
+        [distance, angle, stop] = self.getLimeData()
         #phase 1 - angle tuning
-        if abs(angle) > math.radians(1):
-            if angle > 0:
-                self.right.set(-0.2)
-                self.left.set(0.2)
-            elif angle < 0:
-                self.right.set(0.2)
-                self.left.set(-0.2)
-            else:
-                print('error 01')
-        #phase 2 - distance tuning
-        elif abs(angle) <= math.radians(1):
-            if distance > 0:
-                self.right.set(0.3)
-                self.left.set(0.3)
+        if not stop:
+            if abs(angle) > 5:
+                if angle > 0:
+                    self.right.set(-0.01 * angle - 0.03)
+                    self.left.set(0.01 * angle + 0.03)
+                elif angle < 0:
+                    self.right.set(-0.01 * angle + 0.03)
+                    self.left.set(0.01 * angle - 0.03)
+                else:
+                    print('error 01')
+            #phase 2 - distance tuning
+            elif abs(angle) <= 5:
+                if distance > 0:
+                    self.right.set(0.6)
+                    self.left.set(0.6)
+        else:
+            self.right.set(0)
+            self.left.set(0)
 
 if __name__ == "__main__":
     wpilib.run(MyRobot)
--- Testing/NeoTest/robot.py
+++ Testing/NeoTest/robot.py
@@ -8,16 +8,17 @@ class MyRobot(wpilib.TimedRobot):
     """ Small program to verify Neo motor and encoder are working. """
 
     def robotInit(self):
-      #self.neo: CANSparkMax = CANSparkMax(5, rev.MotorType.kBrushless)
-      self.neo: Spark = Spark(0)
-      sd.putNumber("Neo Power", 0.5)
+        self.neo: CANSparkMax = CANSparkMax(5, rev.MotorType.kBrushless)
+        self.neo.clearFaults()
+        #self.neo: Spark = Spark(0)
+        sd.putNumber("Neo Power", 0.5)
 
     def robotPeriodic(self):
-      #sd.putNumber("Neo Encoder", self.neo.getEncoder().getPosition())
-      pass
-      
-    def teleopPeriodic(self):
-      self.neo.set(sd.getNumber("Neo Power", 0.5))
+        pass
 
-if  __name__ == '__main__':
+    def teleopPeriodic(self):
+        sd.putNumber("Neo Encoder", self.neo.getEncoder().getPosition())
+        self.neo.set(sd.getNumber("Neo Power", 0.5))
+
+if __name__ == '__main__':
     wpilib.run(MyRobot)
